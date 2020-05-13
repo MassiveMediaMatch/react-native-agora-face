714c3a384a168267b43e0400af2edc7549d59696
Tristan Peeters <tristan@massivemedia.eu>
2020-05-13 14:54:01 +0200

    doesn't exist

--- android/src/main/java/live/ablo/agora/ReactNativeAgoraFacePackage.java
+++ android/src/main/java/live/ablo/agora/ReactNativeAgoraFacePackage.java
@@ -18,11 +18,6 @@ public class ReactNativeAgoraFacePackage implements ReactPackage {
 	}
 
 	@Override
-	public List<Class<? extends JavaScriptModule>> createJSModules() {
-		return null;
-	}
-
-	@Override
 	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
 		return Collections.emptyList();
 	}
