249313ebab03a9144e32fd305e0070d12b3f71a8
Jovan Stanimirovic <jovan@netlog.com>
2020-05-15 15:09:00 +0200

    added ios module with correct name

---
+++ ios/RCTReactNativeAgoraFace.h
@@ -0,0 +1,20 @@
+//
+//  ReactNativeAgoraFace.h
+//  ReactNativeAgoraFace
+//
+//  Created by 邓博 on 2017/6/13.
+//  Copyright © 2017年 Syan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <AgoraRtcKit/AgoraRtcEngineKit.h>
+
+@interface ReactNativeAgoraFace : RCTEventEmitter<RCTBridgeModule, AgoraMediaMetadataDelegate, AgoraMediaMetadataDataSource>
+- (void) sendEvent:(NSString *_Nullable)msg params:(NSDictionary *_Nullable)params;
+- (NSInteger) metadataMaxSize;
+- (NSData *_Nullable)readyToSendMetadataAtTimestamp:(NSTimeInterval)timestamp;
+- (void)receiveMetadata:(NSData *_Nonnull)data fromUser:(NSInteger)uid atTimestamp:(NSTimeInterval)timestamp;
+@end
