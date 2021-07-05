5d685236d73791a0442d8b20658fdf91c96c1869
Thomas De Backer <thomas@massivemedia.eu>
2021-07-05 15:19:41 +0200

    fix isSpeakerphoneEnabled method

--- android/src/main/java/live/ablo/agora/AgoraModule.java
+++ android/src/main/java/live/ablo/agora/AgoraModule.java
@@ -585,7 +585,7 @@ public class AgoraModule extends ReactContextBaseJavaModule {
 	}
 
 	@ReactMethod
-	public void methodisSpeakerphoneEnabled(Callback callback) {
+	public void isSpeakerphoneEnabled(Callback callback) {
 		WritableMap map = Arguments.createMap();
 		map.putBoolean("status", AgoraManager.getInstance().getEngine().isSpeakerphoneEnabled());
 		callback.invoke(map);
